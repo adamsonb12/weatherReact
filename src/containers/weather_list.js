35438e1875a42a4ad0b967f0f814b410db2acf6c
Adamson <brett.adamson@pnnl.gov>
2017-05-23 15:48:59 -0700

    styled the charts and whole app

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,13 +6,15 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const temps = cityData.list.map(weather => weather.main.temp)
+        const pressures = cityData.list.map(weather => weather.main.pressure)
+        const hums = cityData.list.map(weather => weather.main.humidity)
 
         return (
             <tr key={cityData.city.name}>
                 <td>{cityData.city.name}</td>
-                <td>
-                    <Chart data={temps} color="blue" />
-                </td>
+                <td><Chart data={temps} color="blue"/></td>
+                <td><Chart data={pressures} color="green"/></td>
+                <td><Chart data={hums} color="orange"/></td>
             </tr>
         )
     }
@@ -23,9 +25,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (Kelvin) </th>
+                        <th>Pressure (hPA) </th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
